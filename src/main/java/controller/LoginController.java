a39b801306d5e73b653a7c8cbf62b60f8718775b
CooolRyan <74342019+CooolRyan@users.noreply.github.com>
2025-02-06 08:41:34 +0900

    FEAT : Recommend Added

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -5,29 +5,20 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import repository.RecommendRepository;
-
 import java.io.IOException;
-import java.util.ArrayList;
 
-import domain.Recommend;
-
-@WebServlet("/recommend")
-public class RecommendController extends HttpServlet {
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public RecommendController() {
+    public LoginController() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try{
-			ArrayList<Recommend> ra = RecommendRepository.getRecommend();
-			request.setAttribute("RecommendArray", ra);
-			request.getRequestDispatcher("views/jsp/recommend.jsp").forward(request, response);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/java/repository/RecommendRepository.java
+++ src/main/java/repository/RecommendRepository.java
@@ -1,5 +1,32 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import domain.Recommend;
+import utils.DBConnection;
+
 public class RecommendRepository {
+	public static ArrayList<Recommend> getRecommend() throws Exception{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<Recommend> all = null;
+		try {
+			con = DBConnection.getConnection();
+			pstmt = con.prepareStatement("select * from recommend");
+			rs = pstmt.executeQuery();
+			
+			all = new ArrayList<>();
+			while(rs.next()) {
+				all.add(new Recommend(rs.getString("title"), rs.getString("description"),rs.getInt("priority")));
+			}
+			return all;
+		}finally {
+			DBConnection.close(con, pstmt, rs);
+		}
+	}
 
 }
