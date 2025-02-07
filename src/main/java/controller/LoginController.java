1caec124ed5846c37781959d252606b79024a205
Park ji hye <parkco011226@gmail.com>
2025-02-07 15:20:42 +0900

    commit

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -19,17 +18,32 @@ public class LoginController extends HttpServlet {
 		String pw = request.getParameter("pw");
 
 		// 고유 세션이 있으면 재사용, 없으면 새로 만듬
-		HttpSession session = request.getSession();
-		System.out.println("Session ID: " + session.getId());
 
 		// id,pw값 각각의 키에 저장
-		session.setAttribute("idkey", id);
-		session.setAttribute("pwkey", pw);
-		session.setAttribute("namekey", "박지혜");
+		
+		
+		
+	
+			HttpSession session = request.getSession();
+			System.out.println("Session ID: " + session.getId());
+			session.setAttribute("idkey", id);
+			session.setAttribute("pwkey", pw);
+			session.setAttribute("namekey", "박지혜");
 
-		System.out.println("로그인 ID: " + session.getAttribute("idkey"));
-
-		response.sendRedirect("main.jsp");
+			System.out.println("로그인 ID: " + session.getAttribute("idkey"));
+			
+			
+			response.sendRedirect("main");
+		
+		}
+	
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        try {
+	            request.getRequestDispatcher("/views/jsp/login.jsp").forward(request, response); //
+	        } catch(Exception e) {
+	            e.printStackTrace();
+	        }
+	    
 
 	}
 
