ef2e75229daf60222398a98ba7063e522d399d36
김리영 (Kim Ri-yeong) <fldud6522@gmail.com>
2025-02-10 12:14:59 +0900

    Feat : Main Page Update

--- src/main/java/repository/MainRepository.java
+++ src/main/java/repository/MainRepository.java
@@ -12,14 +12,16 @@ import utils.DBConnection;
 
 public class MainRepository {
 
-    // 전체 게시글 조회 메서드
-    public static List<Post> getAllPosts() {
+    //가족에 속한 모든 게시글 조회
+    public static List<Post> getPostsByFamilyId(int fid) {
         List<Post> postList = new ArrayList<>();
-        String sql = "SELECT * FROM Post";
+        String sql = "SELECT * FROM Post WHERE fid = ? ORDER BY start_date DESC";
 
         try (Connection conn = DBConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql);
-             ResultSet rs = stmt.executeQuery()) {
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, fid);
+            ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
                 Post post = new Post(
